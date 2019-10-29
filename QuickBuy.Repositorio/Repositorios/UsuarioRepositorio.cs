ddb236790f7bb6d93143350dd40993cbf250ec46
gegadear <gegadear@gmail.com>
2019-10-29 16:01:20 -0300

    Enviar alterações 291020191601

---
+++ QuickBuy.Repositorio/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class UsuarioRepositorio : BaseRepositorio<Usuario>, IUsuarioRepositorio
+    {
+        public UsuarioRepositorio()
+        {
+
+        }
+
+        internal void Adicionar(UsuarioRepositorio usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
